4f639dd6cd85e249e433a7b2b50252dc75f666f5
Josh Searle <joshryansearle@gmail.com>
2021-05-14 14:24:47 +1000

    Add ts index file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './styles/styles.css';
+import classes from './styles/classes';
+
+const root = document.getElementById('root')!;
+root.innerText = 'hello, world';
+root.classList.add(classes.coolStyle);
